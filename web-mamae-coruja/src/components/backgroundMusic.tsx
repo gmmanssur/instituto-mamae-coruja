e936b8546ca6b353ead843b510b98580860a1cc7
Gustavo Manssur <contato.gmanssur@gmail.com>
2026-04-20 09:16:08 -0300

    V35: music position

--- web-mamae-coruja/src/components/backgroundMusic.tsx
+++ web-mamae-coruja/src/components/backgroundMusic.tsx
@@ -95,7 +95,7 @@ export function BackgroundMusic() {
         preload="auto"
       />
 
-      <div className="fixed bottom-6 left-1/2 -translate-x-1/2 z-50 flex items-center gap-3">
+      <div className="fixed bottom-6 left-4 z-50 flex items-center gap-3">
         
         {/* voltar */}
         <button
